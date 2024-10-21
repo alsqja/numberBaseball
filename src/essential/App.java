44ac51c7a207c24d3d77f8b53a31041c9deda6f7
alsqja <alsqha2626@gmail.com>
2024-10-21 14:14:22 +0900

    fix number creater not use hashset

--- src/essential/App.java
+++ src/essential/App.java
@@ -1,4 +1,10 @@
 package essential;
 
 public class App {
+    public static void main(String[] args) {
+        CreateNum createNum = new CreateNum();
+        String number = createNum.getNumber();
+
+        System.out.println(number);
+    }
 }
--- src/essential/CreateNum.java
+++ src/essential/CreateNum.java
@@ -1,22 +1,15 @@
 package essential;
 
-import java.util.HashSet;
-import java.util.Set;
-
 public class CreateNum {
-    private String number;
+    private String number = "";
 
     public CreateNum() {
-        //  hashSet 을 이용해 중복 자동 배제 및 숫자 선정
-        Set<String> numberSet = new HashSet<>();
-        while (numberSet.size() < 3) {
+        while (this.number.length() < 3) {
             String num = (int) (Math.random() * 9 + 1) + "";
-            numberSet.add(num);
-        }
 
-        //  hashSet에 있는 값 String으로 붙이기 (순서 랜덤)
-        while (numberSet.iterator().hasNext()) {
-            this.number += numberSet.iterator().next();
+            if (!this.number.contains(num)) {
+                this.number += num;
+            }
         }
     }
 
